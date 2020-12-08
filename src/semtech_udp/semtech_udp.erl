bcb7e96b1765f8132fe9681d73638e57545f3c34
macpie <pierre@helium.com>
2020-12-07 16:33:21 -0800

    Add semtech pull_resp and tx_ack

--- src/semtech_udp/semtech_udp.erl
+++ src/semtech_udp/semtech_udp.erl
@@ -17,6 +17,9 @@
     push_ack/1,
     pull_data/2,
     pull_ack/1,
+    pull_resp/2,
+    pull_resp/2,
+    tx_ack/3,
     token/0,
     token/1,
     identifier/1
@@ -83,6 +86,49 @@ pull_data(Token, MAC) ->
 -spec pull_ack(binary()) -> binary().
 pull_ack(Token) ->
     <<?PROTOCOL_2:8/integer-unsigned, Token:2/binary, ?PULL_ACK:8/integer-unsigned>>.
+
+%%%-------------------------------------------------------------------
+%% @doc
+%% That packet type is used by the server to send RF packets and associated
+%% metadata that will have to be emitted by the gateway.
+%% @end
+%%%-------------------------------------------------------------------
+-spec pull_resp(
+    binary(),
+    map()
+) -> binary().
+%% TODO: We might need to expand this to add the gateway MAC in there otherwise
+%% I don't know where to send donwlink
+pull_resp(Token, Map) ->
+    BinJSX = jsx:encode(#{txpk => Map}),
+    <<?PROTOCOL_2:8/integer-unsigned, Token/binary, ?PULL_RESP:8/integer-unsigned, BinJSX/binary>>.
+
+%%%-------------------------------------------------------------------
+%% @doc
+%% That packet type is used by the gateway to send a feedback to the server
+%% to inform if a downlink request has been accepted or rejected by the gateway.
+%% The datagram may optionnaly contain a JSON string to give more details on
+%% acknoledge. If no JSON is present (empty string), this means than no error
+%% occured.
+%% @end
+%%%-------------------------------------------------------------------
+-spec tx_ack(
+    binary(),
+    binary()
+) -> binary().
+tx_ack(Token, MAC) ->
+    tx_ack(Token, MAC, <<"NONE">>).
+
+-spec tx_ack(
+    binary(),
+    binary(),
+    binary()
+) -> binary().
+tx_ack(Token, MAC, Error) ->
+    Map = #{error => Error},
+    BinJSX = jsx:encode(#{txpk_ack => Map}),
+    <<?PROTOCOL_2:8/integer-unsigned, Token/binary, ?TX_ACK:8/integer-unsigned, MAC:8/binary,
+        BinJSX/binary>>.
 
 -spec token() -> binary().
 token() ->
@@ -179,6 +225,64 @@ pull_ack_test() ->
     ),
     ok.
 
+pull_resp_test() ->
+    Token0 = token(),
+    Map0 = #{
+        imme => true,
+        freq => 864.123456,
+        rfch => 0,
+        powe => 14,
+        modu => <<"LORA">>,
+        datr => <<"SF11BW125">>,
+        codr => <<"4/6">>,
+        ipol => false,
+        size => 32,
+        data => <<"H3P3N2i9qc4yt7rK7ldqoeCVJGBybzPY5h1Dd7P7p8v">>
+    },
+    PullResp = pull_resp(Token0, Map0),
+    <<?PROTOCOL_2:8/integer-unsigned, Token1:2/binary, Id:8/integer-unsigned, BinJSX/binary>> =
+        PullResp,
+    ?assertEqual(Token1, Token0),
+    ?assertEqual(?PULL_RESP, Id),
+    ?assertEqual(
+        #{
+            <<"txpk">> => #{
+                <<"imme">> => true,
+                <<"freq">> => 864.123456,
+                <<"rfch">> => 0,
+                <<"powe">> => 14,
+                <<"modu">> => <<"LORA">>,
+                <<"datr">> => <<"SF11BW125">>,
+                <<"codr">> => <<"4/6">>,
+                <<"ipol">> => false,
+                <<"size">> => 32,
+                <<"data">> => <<"H3P3N2i9qc4yt7rK7ldqoeCVJGBybzPY5h1Dd7P7p8v">>
+            }
+        },
+        jsx:decode(BinJSX)
+    ),
+    ok.
+
+tx_ack_test() ->
+    Token0 = token(),
+    MAC0 = crypto:strong_rand_bytes(8),
+    TxnAck = tx_ack(Token0, MAC0),
+    <<?PROTOCOL_2:8/integer-unsigned, Token1:2/binary, Id:8/integer-unsigned, MAC1:8/binary,
+        BinJSX/binary>> =
+        TxnAck,
+    ?assertEqual(Token0, Token1),
+    ?assertEqual(?TX_ACK, Id),
+    ?assertEqual(MAC0, MAC1),
+    ?assertEqual(
+        #{
+            <<"txpk_ack">> => #{
+                <<"error">> => <<"NONE">>
+            }
+        },
+        jsx:decode(BinJSX)
+    ),
+    ok.
+
 token_test() ->
     Token = token(),
     ?assertEqual(2, erlang:byte_size(Token)),
@@ -197,6 +301,8 @@ token_test() ->
     ?assertEqual(Token, token(push_ack(Token))),
     ?assertEqual(Token, token(pull_data(Token, crypto:strong_rand_bytes(8)))),
     ?assertEqual(Token, token(pull_ack(Token))),
+    ?assertEqual(Token, token(pull_resp(Token, #{}))),
+    ?assertEqual(Token, token(tx_ack(Token, crypto:strong_rand_bytes(8)))),
     ?assertException(error, function_clause, token(<<"some unknown stuff">>)),
     ok.
 
@@ -217,6 +323,8 @@ identifier_test() ->
     ?assertEqual(?PUSH_ACK, identifier(push_ack(Token))),
     ?assertEqual(?PULL_DATA, identifier(pull_data(Token, crypto:strong_rand_bytes(8)))),
     ?assertEqual(?PULL_ACK, identifier(pull_ack(Token))),
+    ?assertEqual(?PULL_RESP, identifier(pull_resp(Token, #{}))),
+    ?assertEqual(?TX_ACK, identifier(tx_ack(Token, crypto:strong_rand_bytes(8)))),
     ?assertException(error, function_clause, token(<<"some unknown stuff">>)),
     ok.
 
