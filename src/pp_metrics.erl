45dade49a0721d13630f8ca3aff59dab5e548a27
Michael Jeffrey <michaeldjeffrey@gmail.com>
2021-10-01 15:25:41 -0700

    use macros for metrics

--- src/pp_metrics.erl
+++ src/pp_metrics.erl
@@ -7,6 +7,10 @@
 
 -define(ETS, pp_metrics_ets).
 
+-define(METRIC_OFFER_COUNT, packet_purchaser_offer_count).
+-define(METRIC_PACKET_COUNT, packet_purchaser_packet_count).
+-define(METRIC_GWMP_COUNT, packet_purchaser_gwmp_counter).
+
 %% gen_server API
 -export([start_link/0]).
 
@@ -60,7 +64,7 @@ start_link() ->
 handle_offer(PubKeyBin, NetID, OfferType, Action, PayloadSize) ->
     {ok, AName} = animal_name(PubKeyBin),
     DC = calculate_dc_amount(PayloadSize),
-    prometheus_counter:inc(packet_purchaser_offer_count, [AName, NetID, OfferType, Action, DC]).
+    prometheus_counter:inc(?METRIC_OFFER_COUNT, [AName, NetID, OfferType, Action, DC]).
 
 -spec handle_packet(
     PubKeyBin :: libp2p_crypto:pubkey_bin(),
@@ -69,27 +73,27 @@ handle_offer(PubKeyBin, NetID, OfferType, Action, PayloadSize) ->
 ) -> ok.
 handle_packet(PubKeyBin, NetID, PacketType) ->
     {ok, AName} = animal_name(PubKeyBin),
-    prometheus_counter:inc(packet_purchaser_packet_count, [AName, NetID, PacketType]).
+    prometheus_counter:inc(?METRIC_PACKET_COUNT, [AName, NetID, PacketType]).
 
 -spec push_ack(PubKeyBin :: libp2p_crypto:pubkey_bin(), NetID :: non_neg_integer()) -> ok.
 push_ack(PubKeyBin, NetID) ->
     {ok, AName} = animal_name(PubKeyBin),
-    prometheus_counter:inc(packet_purchaser_gwmp_counter, [AName, NetID, push_ack, hit]).
+    prometheus_counter:inc(?METRIC_GWMP_COUNT, [AName, NetID, push_ack, hit]).
 
 -spec push_ack_missed(PubKeyBin :: libp2p_crypto:pubkey_bin(), NetID :: non_neg_integer()) -> ok.
 push_ack_missed(PubKeyBin, NetID) ->
     {ok, AName} = animal_name(PubKeyBin),
-    prometheus_counter:inc(packet_purchaser_gwmp_counter, [AName, NetID, push_ack, miss]).
+    prometheus_counter:inc(?METRIC_GWMP_COUNT, [AName, NetID, push_ack, miss]).
 
 -spec pull_ack(PubKeyBin :: libp2p_crypto:pubkey_bin(), NetID :: non_neg_integer()) -> ok.
 pull_ack(PubKeyBin, NetID) ->
     {ok, AName} = animal_name(PubKeyBin),
-    prometheus_counter:inc(packet_purchaser_gwmp_counter, [AName, NetID, pull_ack, hit]).
+    prometheus_counter:inc(?METRIC_GWMP_COUNT, [AName, NetID, pull_ack, hit]).
 
 -spec pull_ack_missed(PubKeyBin :: libp2p_crypto:pubkey_bin(), NetID :: non_neg_integer()) -> ok.
 pull_ack_missed(PubKeyBin, NetID) ->
     {ok, AName} = animal_name(PubKeyBin),
-    prometheus_counter:inc(packet_purchaser_gwmp_counter, [AName, NetID, pull_ack, miss]).
+    prometheus_counter:inc(?METRIC_GWMP_COUNT, [AName, NetID, pull_ack, miss]).
 
 %% -------------------------------------------------------------------
 %% gen_server Callbacks
@@ -151,14 +155,14 @@ declare_metrics() ->
     %% type = frame type :: join | packet
     %% status = bought :: accepted | rejected
     prometheus_counter:declare([
-        {name, packet_purchaser_offer_count},
+        {name, ?METRIC_OFFER_COUNT},
         {help, "Offer count for NetID"},
         {labels, [animal_name, net_id, type, status, dc]}
     ]),
 
     %% type = frame type :: join | packet
     prometheus_counter:declare([
-        {name, packet_purchaser_packet_count},
+        {name, ?METRIC_PACKET_COUNT},
         {help, "Packet count for NetID"},
         {labels, [animal_name, net_id, type]}
     ]),
@@ -166,7 +170,7 @@ declare_metrics() ->
     %% type = gwmp packet type :: push_ack | pull_ack
     %% status = received :: hit | miss
     prometheus_counter:declare([
-        {name, packet_purchaser_gwmp_counter},
+        {name, ?METRIC_GWMP_COUNT},
         {help, "Semtech UDP acks for Gateway and NetID"},
         {labels, [animal_name, net_id, type, status]}
     ]),
