619ed9a28a45bb0b7397660b0493bce1d26bc3f7
Michael Jeffrey <michaeldjeffrey@gmail.com>
2021-10-01 15:05:01 -0700

    add net_id to suite state

--- test/pp_udp_worker_SUITE.erl
+++ test/pp_udp_worker_SUITE.erl
@@ -27,6 +27,7 @@
     blockchain :: blockchain:blockchain() | undefined,
     location :: {pos_integer(), float(), float()} | undefined,
     pubkeybin :: libp2p_crypto:pubkey_bin(),
+    net_id :: non_neg_integer(),
     socket :: pp_udp_socket:socket(),
     address :: inet:socket_address() | inet:hostname(),
     port :: inet:port_number(),
