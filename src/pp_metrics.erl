0dc21df1771d4a94c57700c841c238bb06c3c671
Michael Jeffrey <michaeldjeffrey@gmail.com>
2021-10-12 11:03:27 -0700

    add more metrics
    - dcs
    - state channels
    - blocks

--- src/pp_metrics.erl
+++ src/pp_metrics.erl
@@ -10,6 +10,17 @@
 -define(METRIC_OFFER_COUNT, packet_purchaser_offer_count).
 -define(METRIC_PACKET_COUNT, packet_purchaser_packet_count).
 -define(METRIC_GWMP_COUNT, packet_purchaser_gwmp_counter).
+-define(METRIC_DC_BALANCE, packet_purchaser_dc_balance).
+-define(METRIC_CHAIN_BLOCKS, packet_purchaser_blockchain_blocks).
+
+-define(METRIC_SC_OPENED_COUNT, packet_purchaser_state_channel_opened_count).
+-define(METRIC_SC_OVERSPENT_COUNT, packet_purchaser_state_channel_overspent_count).
+-define(METRIC_SC_ACTIVE_COUNT, packet_purchaser_state_channel_active_count).
+-define(METRIC_SC_ACTIVE_BALANCE, packet_purchaser_state_channel_active_balance).
+-define(METRIC_SC_ACTIVE_ACTORS, packet_purchaser_state_channel_active_actors).
+
+-define(METRICS_TICK_INTERVAL, timer:seconds(10)).
+-define(METRICS_TICK, '__pp_metrics_tick').
 
 %% gen_server API
 -export([start_link/0]).
@@ -22,7 +33,11 @@
     pull_ack/2,
     pull_ack_missed/2,
     push_ack/2,
-    push_ack_missed/2
+    push_ack_missed/2,
+    %% Stats
+    dcs/1,
+    blocks/1,
+    state_channels/5
 ]).
 
 %% gen_server callbacks
@@ -41,7 +56,7 @@
     handle_event/3
 ]).
 
--record(state, {}).
+-record(state, {pubkey_bin :: libp2p_crypto:pubkey_bin()}).
 
 %% -------------------------------------------------------------------
 %% API Functions
@@ -95,6 +110,28 @@ pull_ack_missed(PubKeyBin, NetID) ->
     {ok, AName} = animal_name(PubKeyBin),
     prometheus_counter:inc(?METRIC_GWMP_COUNT, [AName, NetID, pull_ack, miss]).
 
+-spec dcs(Balance :: non_neg_integer()) -> ok.
+dcs(Balance) ->
+    prometheus_gauge:set(?METRIC_DC_BALANCE, Balance).
+
+-spec blocks(RelativeHeight :: integer()) -> ok.
+blocks(RelativeHeight) ->
+    prometheus_gauge:set(?METRIC_CHAIN_BLOCKS, RelativeHeight).
+
+-spec state_channels(
+    OpenedCount :: non_neg_integer(),
+    OverspentCount :: non_neg_integer(),
+    ActiveCount :: non_neg_integer(),
+    TotalDCLeft :: non_neg_integer(),
+    TotalActors :: non_neg_integer()
+) -> ok.
+state_channels(OpenedCount, OverspentCount, ActiveCount, TotalDCLeft, TotalActors) ->
+    prometheus_gauge:set(?METRIC_SC_OPENED_COUNT, OpenedCount),
+    prometheus_gauge:set(?METRIC_SC_OVERSPENT_COUNT, OverspentCount),
+    prometheus_gauge:set(?METRIC_SC_ACTIVE_COUNT, ActiveCount),
+    prometheus_gauge:set(?METRIC_SC_ACTIVE_BALANCE, TotalDCLeft),
+    prometheus_gauge:set(?METRIC_SC_ACTIVE_ACTORS, TotalActors).
+
 %% -------------------------------------------------------------------
 %% gen_server Callbacks
 %% -------------------------------------------------------------------
@@ -107,10 +144,14 @@ init(Args) ->
     ],
     {ok, _Pid} = elli:start_link(ElliOpts),
 
+    {ok, PubKey, _, _} = blockchain_swarm:keys(),
+    PubKeyBin = libp2p_crypto:pubkey_to_bin(PubKey),
+
     ok = init_ets(),
     ok = declare_metrics(),
+    _ = schedule_next_tick(),
 
-    {ok, #state{}}.
+    {ok, #state{pubkey_bin = PubKeyBin}}.
 
 handle_call(_Request, _From, State) ->
     {reply, ignored, State}.
@@ -118,6 +159,15 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+handle_info(?METRICS_TICK, #state{pubkey_bin = PubKeyBin} = State) ->
+    lager:info("running metrics"),
+    erlang:spawn(fun() ->
+        ok = record_dc_balance(PubKeyBin),
+        ok = record_chain_blocks(),
+        ok = record_state_channels()
+    end),
+    _ = schedule_next_tick(),
+    {noreply, State};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -175,6 +225,28 @@ declare_metrics() ->
         {labels, [animal_name, net_id, type, status]}
     ]),
 
+    %% State channels
+    prometheus_gauge:declare([
+        {name, ?METRIC_SC_OPENED_COUNT},
+        {help, "Opened State Channels count"}
+    ]),
+    prometheus_gauge:declare([
+        {name, ?METRIC_SC_OVERSPENT_COUNT},
+        {help, "Overspent State Channels count"}
+    ]),
+    prometheus_gauge:declare([
+        {name, ?METRIC_SC_ACTIVE_COUNT},
+        {help, "Active State Channels count"}
+    ]),
+    prometheus_gauge:declare([
+        {name, ?METRIC_SC_ACTIVE_BALANCE},
+        {help, "Active State Channels balance"}
+    ]),
+    prometheus_gauge:declare([
+        {name, ?METRIC_SC_ACTIVE_ACTORS},
+        {help, "Active State Channels actors"}
+    ]),
+
     ok.
 
 -spec animal_name(PubKeyBin :: libp2p_crypto:pubkey_bin()) -> {ok, string()}.
@@ -203,3 +275,77 @@ get_ledger() ->
         [{Key, Ledger}] ->
             Ledger
     end.
+
+-spec get_chain() -> blockchain:blockchain().
+get_chain() ->
+    Key = blockchain_chain,
+    case ets:lookup(?ETS, Key) of
+        [] ->
+            Chain = blockchain_worker:blockchain(),
+            true = ets:insert(?ETS, {Key, Chain}),
+            Chain;
+        [{Key, Chain}] ->
+            Chain
+    end.
+
+-spec schedule_next_tick() -> reference().
+schedule_next_tick() ->
+    erlang:send_after(?METRICS_TICK_INTERVAL, self(), ?METRICS_TICK).
+
+record_dc_balance(PubKeyBin) ->
+    Ledger = get_ledger(),
+    case blockchain_ledger_v1:find_dc_entry(PubKeyBin, Ledger) of
+        {error, _} ->
+            ok;
+        {ok, Entry} ->
+            Balance = blockchain_ledger_data_credits_entry_v1:balance(Entry),
+            ok = ?MODULE:dcs(Balance)
+    end,
+    ok.
+record_chain_blocks() ->
+    Chain = get_chain(),
+    case blockchain:height(Chain) of
+        {error, _} ->
+            ok;
+        {ok, Height} ->
+            case hackney:get(<<"https://api.helium.io/v1/blocks/height">>, [], <<>>, [with_body]) of
+                {ok, 200, _, Body} ->
+                    CurHeight = kvc:path(
+                        [<<"data">>, <<"height">>],
+                        jsx:decode(Body, [return_maps])
+                    ),
+                    ok = ?MODULE:blocks(CurHeight - Height);
+                _ ->
+                    ok
+            end
+    end,
+
+    ok.
+
+record_state_channels() ->
+    Chain = get_chain(),
+    {ok, Height} = blockchain:height(Chain),
+    {OpenedCount, OverspentCount, _GettingCloseCount} = pp_sc_worker:counts(Height),
+
+    ActiveSCs = maps:values(blockchain_state_channels_server:get_actives()),
+    ActiveCount = erlang:length(ActiveSCs),
+
+    {TotalDCLeft, TotalActors} = lists:foldl(
+        fun(ActiveSC, {DCs, Actors}) ->
+            Summaries = blockchain_state_channel_v1:summaries(ActiveSC),
+            TotalDC = blockchain_state_channel_v1:total_dcs(ActiveSC),
+            DCLeft = blockchain_state_channel_v1:amount(ActiveSC) - TotalDC,
+            %% If SC ran out of DC we should not be counted towards active metrics
+            case DCLeft of
+                0 ->
+                    {DCs, Actors};
+                _ ->
+                    {DCs + DCLeft, Actors + erlang:length(Summaries)}
+            end
+        end,
+        {0, 0},
+        ActiveSCs
+    ),
+
+    ok = ?MODULE:state_channels(OpenedCount, OverspentCount, ActiveCount, TotalDCLeft, TotalActors),
+    ok.
