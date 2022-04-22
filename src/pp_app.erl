f4484327de56b637f565de436b5ffeaac622b894
Brandon Phillips <brandon@helium.com>
2022-04-22 16:12:28 -0700

    Make TLS usage configurable in the listener
    This commit reworks the process of starting a cowboy listener so that
    the choice to use TLS versus a cleartext cowboy listener is
    configurable.

--- src/pp_app.erl
+++ src/pp_app.erl
@@ -7,6 +7,8 @@
 
 -behaviour(application).
 
+-define(LISTENER, pp_listener).
+
 %% Application callbacks
 -export([start/2, stop/1]).
 
@@ -15,6 +17,8 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    ok = start_listener(),
+
     case pp_sup:start_link() of
         {error, _} = Error ->
             Error;
@@ -25,8 +29,40 @@ start(_StartType, _StartArgs) ->
 
 %%--------------------------------------------------------------------
 stop(_State) ->
+    stop_listener(),
     ok.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+start_listener() ->
+    {ok, AuthMiddleware} = application:get_env(packet_purchaser, pp_auth_middleware),
+
+    StartListenerFunc =
+        case application:get_env(packet_purchaser, pp_listener_use_tls, false) of
+            true -> fun cowboy:start_tls/3;
+            false -> fun cowboy:start_clear/3
+        end,
+
+    {ok, ListenerProtocolOpts} = application:get_env(packet_purchaser, pp_listener_protocol_opts),
+
+    Dispatch = cowboy_router:compile([{'_', [{"/hello", hello_handler, []}]}]),
+
+    {ok, _} = StartListenerFunc(
+        ?LISTENER,
+        ListenerProtocolOpts,
+        #{
+            env => #{
+                dispatch => Dispatch
+            },
+            middlewares => [
+                cowboy_router,
+                AuthMiddleware,
+                cowboy_handler
+            ]
+        }
+    ),
+    ok.
+
+stop_listener() ->
+    cowboy:stop_listener(?LISTENER).
